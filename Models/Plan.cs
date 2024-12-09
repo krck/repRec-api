9083fe862e90e8827fc9b4465ba2e0407396c816
Peter Baumann <peterbaumann2@gmx.de>
2024-12-09 19:18:07 +0100

    Test query - get plans

--- Models/Plan.cs
+++ Models/Plan.cs
@@ -2,6 +2,7 @@ namespace repRec_api;
 
 public class Plan
 {
+    public int Id { get; set; }
+    public required string Name { get; set; }
     public DateOnly Date { get; set; }
-    public string? Workout { get; set; }
 }
